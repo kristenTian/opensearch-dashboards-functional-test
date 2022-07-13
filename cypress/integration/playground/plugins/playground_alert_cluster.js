48441aa3141086623486b20277f682bb3ad267a0
Tao Liu <liutaoaz@amazon.com>
2022-07-13 08:51:02 -0700

    remote cluster healthy test

--- cypress/integration/playground/plugins/playground_alert_cluster.js
+++ cypress/integration/playground/plugins/playground_alert_cluster.js
@@ -6,11 +6,8 @@
 import { ALERTING_PLUGIN_NAME } from '../../../utils/plugins/alerting-dashboards-plugin/constants';
 import { BASE_PATH } from '../../../utils/base_constants';
 
-const SAMPLE_CLUSTER_METRICS_HEALTH_MONITOR =
-  'sample_cluster_metrics_health_monitor';
 const SAMPLE_CLUSTER_METRICS_NODES_STATS_MONITOR =
   'sample_cluster_metrics_nodes_stats_monitor';
-const SAMPLE_CLUSTER_TRIGGER = 'sample_cluster_trigger';
 const SAMPLE_NODE_TRIGGER = 'sample_node_trigger';
 
 describe('ClusterMetricsMonitor', () => {
@@ -41,56 +38,6 @@ describe('ClusterMetricsMonitor', () => {
     beforeEach(() => {
       // cy.deleteAllMonitors();
       // cy.reload();
-    });
-
-    it('for the Cluster Health API', () => {
-      // Confirm empty monitor list is loaded
-      cy.contains('There are no existing monitors');
-
-      // Go to create monitor page
-      cy.contains('Create monitor').click();
-
-      // Select ClusterMetrics radio card
-      cy.get('[data-test-subj="clusterMetricsMonitorRadioCard"]').click();
-
-      // Wait for input to load and then type in the monitor name
-      cy.get('input[name="name"]').type(SAMPLE_CLUSTER_METRICS_HEALTH_MONITOR);
-
-      // Wait for the API types to load and then type in the Cluster Health API
-      cy.get('[data-test-subj="clusterMetricsApiTypeComboBox"]').type(
-        'cluster health{enter}'
-      );
-
-      // Confirm the Query parameters field is present and described as "optional"
-      cy.contains('Query parameters - optional');
-      cy.get('[data-test-subj="clusterMetricsParamsFieldText"]');
-
-      // Press the 'Run for response' button
-      cy.get('[data-test-subj="clusterMetricsPreviewButton"]').click();
-
-      // Add a trigger
-      cy.contains('Add trigger').click({ force: true });
-
-      // Type in the trigger name
-      cy.get('input[name="triggerDefinitions[0].name"]').type(
-        SAMPLE_CLUSTER_TRIGGER
-      );
-
-      cy.wait(10000);
-      // Remove action
-      cy.get('button:contains("Remove action")').click({ force: true });
-
-      // Click the create button
-      cy.get('button').contains('Create').click();
-
-      // Confirm we can see the new trigger
-      cy.contains(SAMPLE_CLUSTER_TRIGGER);
-
-      // Go back to the Monitors list
-      cy.get('a').contains('Monitors').click();
-
-      // Confirm we can see the created monitor in the list
-      cy.contains(SAMPLE_CLUSTER_METRICS_HEALTH_MONITOR);
     });
 
     it('for the Nodes Stats API', () => {
